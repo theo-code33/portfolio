62e843a6137fc8b8ef96d8e2da6f54db95e9c94f
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-03 23:24:30 +0100

    [FIX] Change title of home page

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Home - Théo Gillet - Développeur Front-End',
+    title: 'Théo Gillet - Développeur Web Front-End basé à Bordeaux (33)',
     htmlAttrs: {
       lang: 'fr'
     },
