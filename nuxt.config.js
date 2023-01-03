b52f398c7e77269f8065ce53b311e5e9f0ac9b46
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-03 21:03:06 +0100

    [ADD] Add GA4, Google Search console and init sitemap

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,9 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   server: {
-    host: '0.0.0.0',
+    host: '0.0.0.0'
   },
 
   // Target: https://go.nuxtjs.dev/config-target
@@ -18,11 +18,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: "Hello. Moi, c’est Théo, Développeur Web Freelance basé à Bordeaux (33). Vous êtes au bon endroit si vous cherchez quelqu’un pour créer le site de vos rêves. Vous demandez, je réalise !" },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'robots', name: 'robots', content: 'index, follow' },
+      { hid:'google-site-verification', name:'google-site-verification', content: 'pY5X_BClMgfUBZWknlq9rGAKR_88Ju2pDN40v-ZsIB8' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/image.png' }
+    ],
+    script: [
+      { src: "https://www.googletagmanager.com/gtag/js?id=G-SM0VZ1JK3G", async: true },
+      { src: 'js/ga.js' }
     ]
   },
 
@@ -39,15 +45,39 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    '@nuxtjs/dotenv'
+    '@nuxtjs/dotenv',
+    '@nuxt/image'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/sitemap'
   ],
 
+  sitemap: {
+    hostname: `${process.env.BASE_URL}`,
+    path: '/site-map.xml',
+    gzip: true,
+    routes: [
+      {
+        url: '/',
+        priority: 1,
+        lastMod: '2023-01-02'
+      },
+      {
+        url: '/projets',
+        priority: 1,
+        lastMod: '2023-01-02'
+      },
+      {
+        url: '/contact',
+        priority: 0.5,
+        lastMod: '2023-01-02'
+      }
+    ]
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
